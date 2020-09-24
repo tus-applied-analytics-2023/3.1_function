9e85ca385143d5e4866c0028845bcce55012820e
Hiroshi Kajino <kajino@jp.ibm.com>
2020-09-25 00:13:57 +0900

    initial commit

---
+++ test_main.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+from inspect import signature
+import unittest
+import numpy as np
+from main import identity, double, addition
+
+class TestCase(unittest.TestCase):
+
+    def test_identity(self):
+        sig = signature(identity)
+        self.assertEqual(sig.parameters, 1)
+        for _ in range(10):
+            rv = np.random.randn(1)[0]
+            self.assertEqual(rv, identity(rv))
+
+    def test_double(self):
+        sig = signature(double)
+        self.assertEqual(sig.parameters, 1)
+        for _ in range(10):
+            rv = np.random.randn(1)[0]
+            self.assertEqual(2 * rv, double(rv))
+
+    def test_addition(self):
+        sig = signature(addition)
+        self.assertEqual(sig.parameters, 2)
+        for _ in range(10):
+            rvs = np.random.randn(2)
+            self.assertEqual(rvs[0] + rvs[1], addition(rvs[0], rvs[1]))
+
+if __name__ == "__main__":
+    unittest.main()
