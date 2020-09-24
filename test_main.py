6a85baa298c3e81b515d5cee43eda840946f172d
Hiroshi Kajino <kajino@jp.ibm.com>
2020-09-25 00:15:35 +0900

    fix test

--- test_main.py
+++ test_main.py
@@ -9,21 +9,21 @@ class TestCase(unittest.TestCase):
 
     def test_identity(self):
         sig = signature(identity)
-        self.assertEqual(sig.parameters, 1)
+        self.assertEqual(len(sig.parameters), 1)
         for _ in range(10):
             rv = np.random.randn(1)[0]
             self.assertEqual(rv, identity(rv))
 
     def test_double(self):
         sig = signature(double)
-        self.assertEqual(sig.parameters, 1)
+        self.assertEqual(len(sig.parameters), 1)
         for _ in range(10):
             rv = np.random.randn(1)[0]
             self.assertEqual(2 * rv, double(rv))
 
     def test_addition(self):
         sig = signature(addition)
-        self.assertEqual(sig.parameters, 2)
+        self.assertEqual(len(sig.parameters), 2)
         for _ in range(10):
             rvs = np.random.randn(2)
             self.assertEqual(rvs[0] + rvs[1], addition(rvs[0], rvs[1]))
